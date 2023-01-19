d85a820ee1e7fc0c4522b64c4856dd225e152dd4
Butcherok <butcherok231@gmail.com>
2023-01-19 12:17:31 +0200

    start script & style

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+console.log(document);
+
+const imagesContainer = document.querySelector('.gallery');
+const imagesMarkup = createImageGallery(galleryItems);
+
+imagesContainer.insertAdjacentHTML('beforeend', imagesMarkup);
+imagesContainer.addEventListener('click', onImgClick);
+
+function createImageGallery(galleryItems) {
+    return galleryItems
+    .map(({preview, original, description}) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="large-image.jpg">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </div>
+        `;
+    })
+    .join('');
+};
+function onImgClick(event) {
+    event.preventDefault();
+    if (!event.target.classList.contains('gallery__image')) {
+        return;
+    }
+    return console.log(event.target.dataset.source);
+};
